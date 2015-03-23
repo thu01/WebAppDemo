201045b0246572087f488b1c2b2f943840ec8737
Tianyang Hu <tianyang.hu01@gmail.com>
2015-03-22 18:00:27 -0700

    Fixed page refresh issue

--- public/javascripts/postsApp.js
+++ public/javascripts/postsApp.js
@@ -1,56 +1,74 @@
 'use strict';
 
-var postsApp = angular.module('postsApp', []);
+var postsApp = angular.module('postsApp', ['ui.router']);
 
 //Controler for review page
 postsApp.controller('postsCtrl',[
 '$scope',
 'postsFactory',
 '$rootScope',
-function($scope, postsFactory, $rootScope){
-	$scope.posts = postsFactory.posts;
-    //TODO: Use session data for user name
-  $scope.addPost = function(){
-      if($scope.title === '') { return; }
-      var today = new Date();
-      var dd = today.getDate();
-      var mm = today.getMonth()+1; //January is 0!
-      var yyyy = today.getFullYear();
-      var hour = today.getHours();
-      var minute = today.getMinutes();
-      var second = today.getSeconds();
-      if(dd<10) {
-        dd='0'+dd
-      }
-      if(mm<10) {
-        mm='0'+mm
-      }
-      postsFactory.createPost({
-        post_author: (function () {
-            if ($rootScope.currentUser) {
-              return $rootScope.currentUser.user_name;
-            } else { return 'anonymous' }})(),
-        post_date: mm+'/'+dd+'/'+yyyy,
-        post_date_time: hour+':'+minute+':'+second,
-        post_content: $scope.title
+'$state',
+function($scope, postsFactory, $rootScope, $state){
+  $scope.posts = postsFactory.posts;
+
+  $scope.addPost = function()
+  {
+    if($scope.title === '')
+    { 
+      return; 
+    }
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+    var hour = today.getHours();
+    var minute = today.getMinutes();
+    var second = today.getSeconds();
+    if(dd<10)
+    {
+      dd='0'+dd;
+    }
+    if(mm<10) 
+    {
+      mm='0'+mm;
+    }
+    postsFactory.createPost({
+      post_author: (function () {
+        if ($rootScope.currentUser) 
+        {
+            return $rootScope.currentUser.user_name;
+        } 
+        else
+        { 
+          return 'anonymous'; 
+        }
+      })(),
+      post_date: mm+'/'+dd+'/'+yyyy,
+      post_date_time: hour+':'+minute+':'+second,
+      post_content: $scope.title
       });
-      location.reload();
-    };
+    $state.go($state.current, {}, {reload: true});
+  };
 }]);
 
 postsApp.factory('postsFactory', ['$http', function($http){
-  var o = {
+  var o = 
+  {
     posts: []
   };
-  o.getAll = function() {
-    return $http.get('/posts').success(function(data){
+  o.getAll = function()
+  {
+    return $http.get('/posts').success(function(data)
+    {
       angular.copy(data, o.posts);
     });
   };
-  o.createPost = function(post) {
-  return $http.post('/posts', post).success(function(data){
-    o.posts.push(data);
-  });
-};
+  o.createPost = function(post) 
+  {
+    return $http.post('/posts', post).success(function(data)
+    {
+      o.posts.push(data);
+    });
+  };
   return o;
 }]);
